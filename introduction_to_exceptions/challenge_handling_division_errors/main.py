ad00446d0092282f2527f467f138aea48dda0022
tessadgroot <>
2025-12-04 16:32:26 +0000

    Challenge: Handling Division Errors

--- introduction_to_exceptions/challenge_handling_division_errors/main.py
+++ introduction_to_exceptions/challenge_handling_division_errors/main.py
@@ -1,6 +1,10 @@
 def safe_divide(a, b):
     # Your code here
     try:
-        
-    
+        return a/b
+    except ZeroDivisionError:
+        return 'Cannot divide by zero.'
     pass
+
+print(safe_divide(10,2))
+print(safe_divide(2,0))
